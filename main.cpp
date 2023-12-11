188d8c599789329bab099341d02891a30eaaa8ab
Georgi Slavov <georgi.slavov@egt-bg.com>
2023-12-11 12:00:18 +0200

    [main] added main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: default
+ *
+ * Created on December 11, 2023, 11:52 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
